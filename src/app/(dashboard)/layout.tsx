d2ec5d067873e7fc7dabd78933488fe5de25d445
nikolayddev <122819108+nikolayddev@users.noreply.github.com>
2025-02-18 15:27:41 +0200

    Created side menu design

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,7 @@
+import Menu from "@/components/Menu";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function DasbhoardLayout({
     children,
 }: Readonly<{
@@ -6,7 +10,15 @@ export default function DasbhoardLayout({
     return (
         <div className="h-screen flex">
             {/* Left */}
-            <div className='w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200'></div>
+            <div className='w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200 p-4'>
+                <Link href="/" className="flex items-center justify-center gap-2 lg:justify-start">
+                    <Image src="/logo.png" alt="logo" width={32} height={32}></Image>
+                    <span className="hidden lg:block">SchoolKrasi</span>
+                </Link>
+                <Menu>
+                    
+                </Menu>
+            </div>
             {/* Right */}
             <div className='w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200'></div>
         </div>
