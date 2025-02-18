98f0abcfe4817d23588f8ea485ca18c268dc9cdb
nikolayddev <122819108+nikolayddev@users.noreply.github.com>
2025-02-18 17:21:48 +0200

    Created left bar chart design

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,10 +1,11 @@
+import CountChart from "@/components/CountChart";
 import UserCard from "@/components/UserCard";
 
 export default function AdminPage() {
     return (
         <div className='p-4 flex flex-col gap-4 md:flex-row'>
             {/* Left */}
-            <div className='w-full lg:w-2/3'>
+            <div className='w-full lg:w-2/3 flex flex-col gap-8'>
                 {/* User cards */}
                 <div className='flex gap-4 justify-between flex-wrap'>
                     <UserCard type='student' />
@@ -12,6 +13,17 @@ export default function AdminPage() {
                     <UserCard type='parent' />
                     <UserCard type='staff' />
                 </div>
+                {/* Middle charts */}
+                <div className='flex gap-4 flex-col lg:flex-row'>
+                    {/* Count chart */}
+                    <div className='w-full lg:w-1/3 h-[450px]'>
+                        <CountChart />
+                    </div>
+                    {/* Attendance chart */}
+                    <div className='w-full lg:w-2/3 h-[450px]'></div>
+                </div>
+                {/* Bottom chart */}
+                
             </div>
             {/* Right */}
             <div className='w-full lg:w-1/3'>Right</div>
