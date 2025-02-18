6a65979592d6d9dea017388d14e9df397ffe23fc
nikolayddev <122819108+nikolayddev@users.noreply.github.com>
2025-02-18 16:00:19 +0200

    Created Navbar Design

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -8,7 +9,7 @@ export default function DasbhoardLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <div className="h-screen flex">
+        <div className="h-[100%] flex">
             {/* Left */}
             <div className='w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200 p-4'>
                 <Link href="/" className="flex items-center justify-center gap-2 lg:justify-start">
@@ -16,11 +17,14 @@ export default function DasbhoardLayout({
                     <span className="hidden lg:block">SchoolKrasi</span>
                 </Link>
                 <Menu>
-                    
+
                 </Menu>
             </div>
             {/* Right */}
-            <div className='w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200'></div>
+            <div className='w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll'>
+                <Navbar />
+                {children}
+            </div>
         </div>
     );
 }
