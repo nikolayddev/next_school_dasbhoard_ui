d896bb19e416155fb86548dad445a363b8f56790
nikolayddev <122819108+nikolayddev@users.noreply.github.com>
2025-02-19 12:58:31 +0200

    Calendar, Events and Announcements design

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,7 @@
+import Announcements from "@/components/Announcements";
 import AttendaceChart from "@/components/AttendanceChart";
 import CountChart from "@/components/CountChart";
+import EventCalendar from "@/components/EventCalendar";
 import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
 
@@ -32,7 +34,10 @@ export default function AdminPage() {
                 </div>
             </div>
             {/* Right */}
-            <div className='w-full lg:w-1/3'>Right</div>
+            <div className='w-full lg:w-1/3 flex flex-col gap-8'>
+                <EventCalendar />
+                <Announcements />
+            </div>
         </div>
     );
 }
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -14,7 +14,7 @@ export default function DasbhoardLayout({
             <div className='w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200 p-4'>
                 <Link href="/" className="flex items-center justify-center gap-2 lg:justify-start">
                     <Image src="/logo.png" alt="logo" width={32} height={32}></Image>
-                    <span className="hidden lg:block">SchoolKrasi</span>
+                    <span className="hidden lg:block font-bold">SchoolDDev</span>
                 </Link>
                 <Menu>
 
