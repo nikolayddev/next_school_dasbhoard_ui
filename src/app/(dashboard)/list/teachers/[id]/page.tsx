8ccef9b1048237205bab9ebc755e631772a41658
nikolayddev <122819108+nikolayddev@users.noreply.github.com>
2025-02-20 12:01:33 +0200

    Created SingleTeacher page design

---
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -0,0 +1,107 @@
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BIgCalendar";
+import Performance from "@/components/Performance";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function SingleTeacherPage() {
+    return (
+        <div className='flex-1 p-4 flex flex-col xl:flex-row'>
+            {/* Left */}
+            <div className='w-full xl:w-2/3'>
+                {/* Top */}
+                <div className='flex flex-col lg:flex-row gap-4'>
+                    {/* User Info Card */}
+                    <div className='bg-dSky py-6 px-4 rounded-md flex-1 flex gap-4'>
+                        <div className='w-1/3'>
+                            <Image
+                                src='https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=copress&cs=tinysrgb&w=1200'
+                                alt=''
+                                width={144}
+                                height={144}
+                                className='w-36 h-36 rounded-full object-cover'
+                            />
+                        </div>
+                        <div className='w-2/3 flex flex-col justify-between gap-4'>
+                            <h1 className='text-xl font-semibold'>John Travolta</h1>
+                            <p className='text-sm text-gray-500'>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                            <div className='flex items-center justify-between gap-2 flex-wrap text-xs font-medium'>
+                                <div className='w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2 '>
+                                    <Image src='/blood.png' alt='' width={14} height={14} />
+                                    <span>A+</span>
+                                </div>
+                                <div className='w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2 '>
+                                    <Image src='/date.png' alt='' width={14} height={14} />
+                                    <span>February 2025</span>
+                                </div>
+                                <div className='w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2 '>
+                                    <Image src='/mail.png' alt='' width={14} height={14} />
+                                    <span>user@gmail.com</span>
+                                </div>
+                                <div className='w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2 '>
+                                    <Image src='/phone.png' alt='' width={14} height={14} />
+                                    <span>+1 123 4567</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Small Cards */}
+                    <div className='flex-1 flex gap-4 justify-between flex-wrap'>
+                        {/* Card */}
+                        <div className='bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] lg:w-full xl:w-[45%] 2xl:w-[47%]'>
+                            <Image src='/singleAttendance.png' alt='' width={24} height={24} className='h-6 w-6' />
+                            <div className=''>
+                                <h1 className='text-xl font-semibold'>90%</h1>
+                                <span className='text-sm text-gray-400'>Attendance</span>
+                            </div>
+                        </div>
+                        {/* Card */}
+                        <div className='bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] lg:w-full xl:w-[45%] 2xl:w-[47%]'>
+                            <Image src='/singleClass.png' alt='' width={24} height={24} className='h-6 w-6' />
+                            <div className=''>
+                                <h1 className='text-xl font-semibold'>6</h1>
+                                <span className='text-sm text-gray-400'>Classes</span>
+                            </div>
+                        </div>
+                        {/* Card */}
+                        <div className='bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] lg:w-full xl:w-[45%] 2xl:w-[47%]'>
+                            <Image src='/singleLesson.png' alt='' width={24} height={24} className='h-6 w-6' />
+                            <div className=''>
+                                <h1 className='text-xl font-semibold'>6</h1>
+                                <span className='text-sm text-gray-400'>Lessons</span>
+                            </div>
+                        </div>
+                        {/* Card */}
+                        <div className='bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] lg:w-full xl:w-[45%] 2xl:w-[47%]'>
+                            <Image src='/singleBranch.png' alt='' width={24} height={24} className='h-6 w-6' />
+                            <div className=''>
+                                <h1 className='text-xl font-semibold'>2</h1>
+                                <span className='text-sm text-gray-400'>Branches</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* Bottom */}
+                <div className='mt-4 bg-white rounded-md p-4 h-[800px]'>
+                    <h1>Teacher&apos;s Schedule</h1>
+                    <BigCalendar />
+                </div>
+            </div>
+            {/* Right */}
+            <div className='w-full xl:w-1/3 flex flex-col gap-4'>
+                <div className='bg-white p-4 rounded-md'>
+                    <h1 className='text-xl font-semibold'>Shortcuts</h1>
+                    <div className='mt-4 flex gap-4 flex-wrap text-xs text-gray-500'>
+                        <Link className='p-3 rounded-md bg-dSkyLight' href="/">Teacher&apos;s Classes</Link>
+                        <Link className='p-3 rounded-md bg-dPurpleLight' href="/">Teacher&apos;s Students</Link>
+                        <Link className='p-3 rounded-md bg-dYellowLight' href="/">Teacher&apos;s Lessons</Link>
+                        <Link className='p-3 rounded-md bg-pink-50' href="/">Teacher&apos;s Exams</Link>
+                        <Link className='p-3 rounded-md bg-dSkyLight' href="/">Teacher&apos;s Assignments</Link>
+                    </div>
+                </div>
+                <Performance />
+                <Announcements />
+            </div>
+        </div>
+    );
+}
--- src/components/BIgCalendar.tsx
+++ src/components/BIgCalendar.tsx
@@ -1,13 +1,14 @@
-"use client"
+"use client";
 
-import { Calendar, momentLocalizer, View, Views } from 'react-big-calendar'
-import moment from 'moment'
-import { calendarEvents } from '@/lib/data'
-import { useState } from 'react'
+import { Calendar, momentLocalizer, View, Views } from "react-big-calendar";
+import moment from "moment";
+import { calendarEvents } from "@/lib/data";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { useState } from "react";
 
-const localizer = momentLocalizer(moment)
+const localizer = momentLocalizer(moment);
 
-export default function BigCalendar() {
+const BigCalendar = () => {
     const [view, setView] = useState<View>(Views.WORK_WEEK);
 
     const handleOnChangeView = (selectedView: View) => {
@@ -20,12 +21,14 @@ export default function BigCalendar() {
             events={calendarEvents}
             startAccessor="start"
             endAccessor="end"
-            style={{ height: "98%" }}
             views={["work_week", "day"]}
             view={view}
+            style={{ height: "98%" }}
             onView={handleOnChangeView}
-            min={new Date(2025, 2, 0, 8, 0, 0)}
-            max={new Date(2025, 2, 0, 17, 0, 0)}
+            min={new Date(2025, 1, 0, 8, 0, 0)}
+            max={new Date(2025, 1, 0, 17, 0, 0)}
         />
     );
-} 
+};
+
+export default BigCalendar;
